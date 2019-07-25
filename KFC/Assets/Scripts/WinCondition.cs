61d1035b9a0fd8353b77f31d0e19e58a9f6996f6
littleghostprince <ashleysmejia@gmail.com>
2019-07-25 16:33:36 -0600

    7/25
    timer + dstufkljafsfs

---
+++ KFC/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    public static bool gameover = false; 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            gameover = true;
+            Debug.Log("hey condition is: " + gameover);
+        }
+    }
+}
