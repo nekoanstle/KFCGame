3b955dc2c78b14008764711826ab5ec6b8d11196
Neko Anstle <nekoanstle@gmail.com>
2019-07-26 15:52:24 -0600

    Revert "7/26"
    This reverts commit 68fa3cde23e343f0fa3e3a91cbd67859069aa856.

--- KFC/Assets/Scripts/Timer.cs
+++ KFC/Assets/Scripts/Timer.cs
@@ -10,8 +10,6 @@ public class Timer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        totalTime = 0;
-        WinCondition.gameover = false; 
         text = GetComponent<TextMeshProUGUI>();
     }
 
