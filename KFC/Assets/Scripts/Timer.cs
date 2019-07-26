68fa3cde23e343f0fa3e3a91cbd67859069aa856
littleghostprince <ashleysmejia@gmail.com>
2019-07-26 14:03:13 -0600

    7/26

--- KFC/Assets/Scripts/Timer.cs
+++ KFC/Assets/Scripts/Timer.cs
@@ -10,6 +10,8 @@ public class Timer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        totalTime = 0;
+        WinCondition.gameover = false; 
         text = GetComponent<TextMeshProUGUI>();
     }
 
