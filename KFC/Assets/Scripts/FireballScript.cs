e9c8cecba91f59082a0a52419c71154bdb1184c2
littleghostprince <ashleysmejia@gmail.com>
2019-07-26 12:21:00 -0600

    7/26

---
+++ KFC/Assets/Scripts/FireballScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballScript : MonoBehaviour
+{
+    //Fireballs
+
+    [SerializeField] public GameObject m_projectile = null;
+    private Rigidbody2D projectile_rb = null;
+    public float velX = 5f;
+    public float velY = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectile_rb = m_projectile.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        projectile_rb.velocity = new Vector2(velX, velY);
+    }
+}
